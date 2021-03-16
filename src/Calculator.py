bcd8c2b053ac22d8ea493dfbc8d7b7c915165f21
garima negi <gn32@njit.edu>
2021-03-16 19:43:52 -0400

    Feature : setup Calculator with division property

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,8 @@
 from addition import addition
 from subtraction import subtraction
 from multiplication import multiplication
+from division import division
+
 
 class Calculator:
     result = 0
@@ -11,14 +13,17 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        self.result = addition(a,b)
+        self.result = addition(a, b)
         return self.result
 
     def sub(self, a, b):
-        self.result = subtraction(a,b)
+        self.result = subtraction(a, b)
         return self.result
 
     def mul(self, a, b):
-        self.result = multiplication(a,b)
+        self.result = multiplication(a, b)
         return self.result
 
+    def div(self, a, b):
+        self.result = division(a, b)
+        return self.result
